2a29df8a8ce92ae12becbbcd4701d01dbe76b1e0
premonish <36532816+premonish@users.noreply.github.com>
2018-05-26 22:44:05 +0800

    Add files via upload
    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,19 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'The best website ever!';
+
+var name = prompt("Hello and welcome! \n\n\n\n\n What is your name please?");
+
+alert(name + ' is a pretty unique and attractive name. You should be proud.');
+
+var age = prompt("What year were you born (if you don't mind my asking)?");
+
+alert((2018 - age) + " is a good age. You are really good at this...");
+
+var password = prompt("Now, " + name + ", we are going to make you a PERSONAL PASSWORD. \n\n\n\n\n What is the name of your favorite dog ever?");
+
+alert('Wow, your password is: \n\n\n\n'+ password + age + '\n\n\n\n Now, I know your personal password and I\'ll keep it private.');
+
+alert("Next, I'll need your credit card info... \n \n\n\n JOKE! \n\n\n\n Enjoy the website! I love you!");
+
+
+
